41d0b5fa9071e85a44a6395786a2fabfeeac4d5c
firsov <firsov@firsov.kievreclama.local>
2019-06-29 09:30:43 +0300

    I'm creating service room

---
+++ informer/src/main/java/informer/service/ServiceRoom.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package informer.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.transaction.Transactional;
+
+import informer.entity.Room;
+import informer.model.ModelRoom;
+import informer.repository.RepositoryRoom;
+import informer.service.convertor.ConvertorBase;
+
+/**
+ * @author firsov
+ *
+ */
+@Stateless
+public class ServiceRoom {
+	
+	@EJB
+	private RepositoryRoom repository;
+	
+	@EJB(beanName = "ConvertorRoom")
+	private ConvertorBase<Room, ModelRoom> convertor;
+	
+	public List<ModelRoom> all(){
+		return repository.all().stream().map(e -> convertor.entityToModel(e)).collect(Collectors.toList());
+	}
+
+	@Transactional
+	public Integer add(ModelRoom model) {
+		return repository.create(convertor.modelToEntity(model)).getId();
+	}
+
+	@Transactional
+	public ModelRoom findId(Integer primaryKey) {
+		return convertor.entityToModel(repository.find(primaryKey));
+	}
+
+
+}
