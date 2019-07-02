797b70726ebbd911d0bb215ea0cbcdbd983f4cb6
firsachi <firsachi@firsbook>
2019-07-02 11:25:59 +0300

    add hash and equals

--- informer/src/main/java/informer/model/ModelRoom.java
+++ informer/src/main/java/informer/model/ModelRoom.java
@@ -53,6 +53,37 @@ public class ModelRoom {
 			return ModelRoom.this;
 		}
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((numberRoom == null) ? 0 : numberRoom.hashCode());
+		result = prime * result + ((roomId == null) ? 0 : roomId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ModelRoom other = (ModelRoom) obj;
+		if (numberRoom == null) {
+			if (other.numberRoom != null)
+				return false;
+		} else if (!numberRoom.equals(other.numberRoom))
+			return false;
+		if (roomId == null) {
+			if (other.roomId != null)
+				return false;
+		} else if (!roomId.equals(other.roomId))
+			return false;
+		return true;
+	}
 
 	@Override
 	public String toString() {
