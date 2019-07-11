0b61082340eed1cd486e2e56749da93bc30e2118
firsov <firsov@firsov.kievreclama.local>
2019-07-11 17:58:22 +0300

    test

---
+++ informer/src/test/java/informer/service/ServicePhoneTest.java
@@ -0,0 +1,34 @@
+package informer.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ServicePhoneTest {
+
+	@Test
+	void testAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindPrimaryKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+}
