52a12cb9ed872b3aa1b6584b0165c654cc0cd1cc
firsov <firsov@firsov.kievreclama.local>
2019-07-15 17:23:03 +0300

    I'm creating junit test

--- informer/src/test/java/informer/service/ServicePhoneTest.java
+++ informer/src/test/java/informer/service/ServicePhoneTest.java
@@ -1,6 +1,7 @@
 package informer.service;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +10,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import informer.entity.Phone;
@@ -23,21 +23,59 @@ public class ServicePhoneTest {
 	@InjectMocks
 	private ServicePhone servicePhone;
 	
+	private Phone testPhone;
+	
 	@Before
     public void initMocks() {
         MockitoAnnotations.initMocks(this);
+        testPhone = new Phone();
+		testPhone.setId(3);
+		testPhone.setNumber("103");
     }
 
 	@Test
 	public void testFindAll() {
+		
 		List<Phone> phones = new ArrayList<Phone>();
-		phones.add(new Phone());
-		Mockito.when(repositoryPhone.all()).thenReturn(phones);
+	    Phone phone = new Phone();
+	    phone.setId(1);
+	    phone.setNumber("101");
+	    phones.add(phone);
+		phone = new Phone();
+		phone.setId(2);
+		phone.setNumber("102");
+		phones.add(phone);
+		
+		when(repositoryPhone.all()).thenReturn(phones);
 		
 		List<Phone> actual = servicePhone.findAll();
-		Mockito.verify(repositoryPhone, Mockito.times(1)).all();
+		verify(repositoryPhone, times(1)).all();
 		assertEquals(phones.size(), actual.size());
 
+	}
+	
+	@Test
+	public void testAdd() {
+		when(repositoryPhone.create(testPhone)).thenReturn(testPhone);
+		Phone actual = servicePhone.add(testPhone);
+		verify(repositoryPhone, times(1)).create(testPhone);
+		assertEquals(testPhone, actual);
+	}
+	
+	@Test
+	public void testFindPrimaryKey() {
+		when(repositoryPhone.find(3)).thenReturn(testPhone);
+		Phone actual = servicePhone.findPrimaryKey(3);
+		verify(repositoryPhone, times(1)).find(3);
+		assertEquals(testPhone, actual);
+	}
+	
+	@Test
+	public void testFindPrimaryKey_NULL() {
+		when(repositoryPhone.find(3)).thenReturn(testPhone);
+		Phone actual = servicePhone.findPrimaryKey(2);
+		verify(repositoryPhone, times(1)).find(2);
+		assertNull(actual);
 	}
 
 }
